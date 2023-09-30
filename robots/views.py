c2c0619c71a0ab7ed05f85cc655e952f91281010
Evgenii <evgeniirg73@gmail.com>
2023-09-30 21:56:42 +0700

    added downloader excel-file for robots_list

--- robots/views.py
+++ robots/views.py
@@ -1,11 +1,18 @@
 import json
+from datetime import timedelta
 from http import HTTPStatus
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+
+from django.db.models import Count
+from django.http import HttpResponse, JsonResponse
+from django.utils import timezone
 from django.utils.decorators import method_decorator
 from django.views import View
-from .models import Robot
+from django.views.decorators.csrf import csrf_exempt
+
+from services.excel_downloader import RobotExcelDownloader
+
 from .forms import RobotForm
+from .models import Robot
 
 
 class RobotAPIView(View):
@@ -27,3 +34,27 @@ class RobotAPIView(View):
                 return JsonResponse({'error': form.errors}, status=HTTPStatus.BAD_REQUEST)
         except AttributeError as e:
             return JsonResponse({'error': str(e)}, status=HTTPStatus.INTERNAL_SERVER_ERROR)
+
+
+class RobotExcelView(View):
+    """Вьюкласс формирования excel-файла с отчетом по производству роботов
+       за последнюю прошедшую неделю."""
+    def get(self, request, *args, **kwargs):
+        current_datetime = timezone.now()
+        start_of_week = current_datetime - timedelta(days=current_datetime.weekday()+7)
+        end_of_week = start_of_week + timedelta(days=7)
+
+        queryset = Robot.objects.filter(
+            created__gte=start_of_week,
+            created__lte=end_of_week
+        ).values('model', 'version').annotate(count=Count('id'))
+
+        excel_downloader = RobotExcelDownloader(queryset)
+        wb = excel_downloader.generate_excel()
+        response = HttpResponse(
+            content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        )
+        response['Content-Disposition'] = 'attachment; filename=robots_counts.xlsx'
+        wb.save(response)
+
+        return response
